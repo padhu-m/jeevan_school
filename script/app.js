f821d17271c5049cd6b70e6c02e3588acba65ff7
padhu-m <padmanaban@hucclecote.com>
2022-04-16 21:25:48 +0530

    Plugin Fix

--- script/app.js
+++ script/app.js
@@ -1,9 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
     },
-  });
+    778: {
+      slidesPerView: 3,
+    }
+  }
+});
